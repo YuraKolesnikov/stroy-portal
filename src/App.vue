46af8dfc2854dd9fc0e2cb2ac8bc578f9f9d9b01
Yurii Kolesnikov <yk@vcv.ru>
2020-10-03 15:11:35 +0700

    Removed About.vue, added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="navbar">
+		<div class="container">
+			<div class="row">
+				<div class="col-4">
+					<div class="navbar__location"></div>
+				</div>
+				<div class="col-5">
+					<ul class="navbar__nav">
+						<li class="navbar__item">
+							<router-link to="/" class="navbar__link">Тендеры</router-link>
+						</li>
+						<li class="navbar__item">
+							<router-link to="/" class="navbar__link">База подрядчиков</router-link>
+						</li>
+						<li class="navbar__item">
+							<router-link to="/" class="navbar__link">Другие сервисы</router-link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.navbar {
+	background-color: $gray;
+}
+</style>
