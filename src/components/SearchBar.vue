4c2bd9f0d08257852de77391e779c1d787affc4d
Yurii Kolesnikov <yk@vcv.ru>
2020-10-03 16:10:26 +0700

    Added SearchBar html and styles

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,14 +1,24 @@
 <template>
 	<div class="search-bar">
 		<div class="container">
-			<div class="row">
-				<div class="col-2">
+			<div class="row align-items-center">
+				<div class="w-20">
 					<img class="search-bar__logo" src="@/assets/img/logo.png" alt="">
 				</div>
-				<div class="col-2">Cat</div>
-				<div class="col-6">INPUT</div>
-				<div class="col-1">LIST</div>
-				<div class="col-1 d-flex justify-content-end">
+				<div class="w-10">
+					<button class="search-bar__catalogue">
+						Каталог
+					</button>
+				</div>
+				<div class="w-50">
+					<text-input placeholder="Хочу найти..." :value="searchValue" @input="updateSearchValue" @clearSearchValue="clearSearchValue" />
+				</div>
+				<div class="w-10">
+					<button class="search-bar__list">
+						Список
+					</button>
+				</div>
+				<div class="w-5">
 					<div class="search-bar__profile-button">
 						A
 					</div>
@@ -18,14 +28,56 @@
 	</div>
 </template>
 
-<style lang="scss" scoped>
-.row div {
-	border: 2px solid tomato;
+<script>
+import TextInput from '@/components/TextInput'
+export default {
+	components: {
+		TextInput
+	},
+	data() {
+		return {
+			searchValue: ''
+		}
+	},
+	methods: {
+		updateSearchValue({ target }) {
+			this.searchValue = target.value
+		},
+		clearSearchValue() {
+			this.searchValue = ''
+		}
+	}
 }
+</script>
 
+<style lang="scss" scoped>
 .search-bar {
+	padding: 18px 0;
+
 	&__logo {
+		max-width: 221px;
+	}
+
+	&__catalogue {
 		width: 100%;
+		height: 100%;
+		border: 0;
+		height: 100%;
+		border-radius: 4px;
+		padding: 8px 16px;
+		font-weight: 600;
+		background-color: $accent-1;
+	}
+
+	&__list {
+		width: 100%;
+		border: 1px solid #ECEFF1;
+		border-radius: 4px;
+		height: 100%;
+		border-radius: 4px;
+		padding: 8px 16px;
+		font-weight: 600;
+		background-color: $light-gray;
 	}
 
 	&__profile-button {
@@ -39,4 +91,23 @@
 		justify-content: center;
 	}
 }
+
+.w-5,
+.w-10,
+.w-20,
+.w-50 {
+	max-height: 40px;
+}
+
+.w-5:not(:last-child),
+.w-10:not(:last-child),
+.w-20:not(:last-child),
+.w-50:not(:last-child) {
+	margin-right: 24px;
+}
+
+.w-5 { flex: 0 0 calc(5% - 12px); }
+.w-10 { flex: 0 0 calc(10% - 12px); }
+.w-20 { flex: 0 0 calc(20% - 12px); }
+.w-50 { flex: 0 0 calc(50% - 12px); }
 </style>
---
+++ src/components/TextInput.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="text-input">
+		<input type="text" class="text-input__input" :placeholder="placeholder" @input="$emit('input', $event)" :value="value">
+		<button class="text-input__button" @click="clearSearchValue">
+			{{ value ? 'C' : 'S' }}
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		placeholder: String,
+		value: String
+	},
+	methods: {
+		clearSearchValue() {
+			if (!this.value) return
+			this.$emit('clearSearchValue')
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.text-input {
+	color: $dark;
+	border: 1px solid #ECEFF1;
+	box-sizing: border-box;
+	border-radius: 4px;
+	width: 100%;
+	padding: 7px;
+	font-weight: 40;
+	font-size: 14px;
+
+	&__input {
+		width: calc(100% - 20px);
+		border: 0;
+		appearance: none;
+
+		&::placeholder {
+			color: inherit;
+			color: $dark-gray;
+		}
+		&:focus {
+			outline: none;
+		}
+	}
+
+	&__button {
+		border: 0;
+		width: 20px;
+		height: 20px;
+		padding: 0;
+		
+		&:focus, &:active {
+			outline: none;
+		}
+	}
+}
+</style>
